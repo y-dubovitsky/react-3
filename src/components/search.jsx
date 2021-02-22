7b787630df68b738474a439eaf4c6d67e1b928c0
user <y.dubovitsky@gmail.com>
2021-02-22 18:53:27 +0300

    Lesson 52 Refactor Search component with Hooks

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,85 +1,68 @@
 import React from 'react';
 
-export default class Search extends React.Component {
+export default function Search({searchFilmByName}) {
 
-    constructor(props) {
-        super(props);
+    const [searchString, setSearchString] = React.useState('');
+    const [type, setType] = React.useState('');
 
+    const onInputText = (event) => {
+        setSearchString(event.target.value)
     }
 
-    state = {
-        searchString: '',
-        type: ''
-    }
-
-    onInputText = (event) => {
-        this.setState({
-            searchString: event.target.value
-        })
-    }
-
-    onSearchText = (event) => {
-        const { searchFilmByName } = this.props;
-
+    const onSearchText = (event) => {
         if (event.key === 'Enter') {
-            searchFilmByName(this.state.searchString, this.state.type)
+            searchFilmByName(searchString, type)
         }
     }
 
-    onFilter = (event) => {
-        this.setState({
-            type: event.target.name
-        })
+    const onFilter = (event) => {
+        setType(event.target.name)
     }
 
-    render() {
-        const { searchString } = this.state;
-
-        return (
-            <div className="row">
-                <div className="col s12">
-                    <div className="input-field">
-                        <input
-                            placeholder="...search film"
-                            type="text"
-                            value={searchString}
-                            onChange={this.onInputText}
-                            onKeyPress={this.onSearchText}
-                        />
-                    </div>
-                </div>
-                <div className="col s12">
-                    <label>
-                        <input
-                            className="with-gap"
-                            onChange={this.onFilter}
-                            checked={this.state.type === 'all'}
-                            name="all"
-                            type="radio" />
-                        <span>All</span>
-                    </label>
-                    <label>
-                        <input
-                            className="with-gap"
-                            onChange={this.onFilter}
-                            checked={this.state.type === 'movie'}
-                            name="movie"
-                            type="radio" />
-                        <span>Films</span>
-                    </label>
-                    <label>
-                        <input
-                            className="with-gap"
-                            onChange={this.onFilter}
-                            checked={this.state.type === 'series'}
-                            name="series"
-                            type="radio" />
-                        <span>Serials</span>
-                    </label>
+    return (
+        <div className="row">
+            <div className="col s12">
+                <div className="input-field">
+                    <input
+                        placeholder="...search film"
+                        type="text"
+                        value={searchString}
+                        onChange={(event) => onInputText(event)}
+                        onKeyPress={(event) => onSearchText(event)}
+                    />
                 </div>
             </div>
-        )
-    }
-
-
+            <div className="col s12">
+                <label>
+                    <input
+                        className="with-gap"
+                        onChange={() => onFilter}
+                        checked={type === 'all'}
+                        name="all"
+                        type="radio" />
+                    <span>All</span>
+                </label>
+                <label>
+                    <input
+                        className="with-gap"
+                        onChange={(event) => onFilter(event)}
+                        checked={type === 'movie'}
+                        name="movie"
+                        type="radio" />
+                    <span>Films</span>
+                </label>
+                <label>
+                    <input
+                        className="with-gap"
+                        onChange={(event) => onFilter(event)}
+                        checked={type === 'series'}
+                        name="series"
+                        type="radio" />
+                    <span>Serials</span>
+                </label>
+            </div>
+        </div>
+    )
 }
+
+
